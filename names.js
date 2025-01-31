799d9daef469bb83f0667eb913ec1c936d4d50d7
GiuntaGiovanni <jhonniest741@gmail.com>
2025-01-31 16:28:02 +0100

    [feat] function + export

---
+++ names.js
@@ -0,0 +1,22 @@
+// 1 - names.js
+// 1. Crea una funzione che accetta due parametri: firstName, lastName. La funzione dovrebbe restituire un oggetto con le proprietà firstName e lastName.
+// 2. Esporta la funzione dal file. 
+
+
+function personalData (nome, cognome){
+    const data = {
+        firstName: nome,
+        lastName: cognome
+    }
+    return data
+
+}
+
+const name = 'Giovanni'
+const surname = 'Giunta'
+
+console.log (personalData(name, surname));
+
+module.exports = {
+    personalData
+}
